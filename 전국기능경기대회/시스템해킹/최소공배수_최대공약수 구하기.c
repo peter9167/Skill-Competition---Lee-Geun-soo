309fe9d965f4dc5117e7f1a6499949748ee9f900
Peter <peter9167@naver.com>
2022-05-31 11:42:44 +0900

    commit

--- 전국기능경기대회/시스템해킹/최소공배수_최대공약수 구하기.c
+++ 전국기능경기대회/시스템해킹/최소공배수_최대공약수 구하기.c
@@ -9,7 +9,7 @@ int main()
 	int a, b, c; // 3개 정수 입력 변수 
    	int g, l; 	 // 최대공약수, 최소공배수 
    	printf("세정수 입력 : ");
-   	scanf("%d %d %d", &a, &b, &c);
+   	scanf("%d %d %d", &a, &b, &c); // 3개 정수 입력 
    
    	g = gcd(a, b, c); // 최대 공약수 값 리턴 
    	printf("최대공약수 : %d\n", g); 
@@ -20,7 +20,7 @@ int main()
    	return 0;
 }
 
-int min(int a, int b, int c)
+int min(int a, int b, int c) // 최소값 리턴 함수 
 {
    int tmp;
    
@@ -52,57 +52,62 @@ int mid(int a, int b, int c)
    return tmp;
 }
 
-int gcd(int a, int b, int c)
+int gcd(int a, int b, int c) // 최대공약수 반환 
 {
-   int g = 1, tmp, i = 2;
-   tmp = min(a, b, c);
+   int g = 1; // 최대공약수 저장 
+   int tmp, i = 2;
+   tmp = min(a, b, c); // 최소값 저장 
    
-   while(i <= tmp){
-      	if(a % i == 0 && b % i == 0 && c % i == 0){
-         	g *= i;
-         	a /= i; b /= i; c /= i;
-         	tmp = min(a,b,c);
+   while(i <= tmp){ 
+   		// 공약수 찾기 
+      	if(a % i == 0 && b % i == 0 && c % i == 0){ // a,b,c 정수가 나눠떨어질 때
+		  	// i가 공약수 
+         	g *= i; // i와 g를 곱하여 저장 - g = g * i
+         	a /= i; b /= i; c /= i; // a,b,c 정수를 i값으로 나눔 
+         	tmp = min(a,b,c); // 최소값 저장 
 
-         	continue;
+         	continue; //while 문으로 올라감 
       	}
-      	i++;
+      	//공약수가 없을 때 
+      	i++; // i +1 증가 
    }
    
-   return g;
+   return g; // i값을 모두 곱한 값을 저장후 리턴(전달) 
 }
 
-int lcm(int a, int b, int c)
+int lcm(int a, int b, int c) // 최소공배수 반환 
 {
-   	int L, tmp, i;
+   	int l; // 최소공배수 저장 
+	int tmp, i;
    
    	tmp = mid(a, b, c);
-   	L = gcd(a, b, c); // 최대공약수 
+   	l = gcd(a, b, c); // 최대공약수 
    
-   	a /= L;  b /= L;  c /= L;
+   	a /= l;  b /= l;  c /= l;
    
    	i=2;
    	while(i <= tmp){
       
       	if(a % i == 0 && b % i == 0){
-         	L *= i;
+         	l *= i;
          	a /= i; b /= i;
          	tmp = mid(a, b, c);
          	continue;
       	}
       	else if(a % i == 0 && c % i == 0){
-         	L *= i;
+         	l *= i;
          	a /= i; c /= i;
          	tmp = mid(a, b, c);
          	continue;
       	}
       
       	else if(a % i == 0 && c % i == 0){
-         	L *= i;
+         	l *= i;
          	a /= i; c /= i;
          	tmp = mid(a, b, c);
          	continue;
       	}
       	i++;
    	}
-   	return L * a * b *c;
+   	return l * a * b *c;
 }
